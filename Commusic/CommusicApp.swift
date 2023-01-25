3211e8355303df2ed2e0b0e841e2bd7db158e759
Juan de Miguel <jdemigueldev@gmail.com>
2023-01-25 13:21:44 +0100

    Initial project upload

---
+++ Commusic/CommusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommusicApp.swift
+//  Commusic
+//
+//  Created by Bluyin  on 24/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CommusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ApiList()
+        }
+    }
+}
