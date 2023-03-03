d04ea36f08223204faa8d56f45437cad132bebd3
Juan de Miguel <jdemigueldev@gmail.com>
2023-03-03 12:35:52 +0100

    Create PostListView

---
+++ Commusic/Page VIews/PostListView.swift
@@ -0,0 +1,44 @@
+
+import SwiftUI
+
+struct PostListView: View {
+    
+    @State var searchText = ""
+    @State var postsList = [Post]()
+    
+    var body: some View {
+        
+        HStack {
+            NavigationView {
+                List(filteredPostsList, id: \.id) { post in
+                    NavigationLink(destination: PostDetail(post: post), label: {
+                        PostCell (post: post)
+                    })
+                }
+                .task {
+                    postsList = await getPosts()
+                }
+            }
+            .listStyle(PlainListStyle())
+            .searchable(text: $searchText)
+        }
+        //.edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        //.preferredColorScheme(.dark)
+    }
+    
+    var filteredPostsList: [Post] {
+        let searchTextLowercased = searchText.lowercased()
+        return searchTextLowercased == "" ? postsList : postsList.filter { post in
+            let titleLowercased = post.title.lowercased()
+            let descriptionLowercased = post.description.lowercased()
+            return titleLowercased.contains(searchTextLowercased) || descriptionLowercased.contains(searchTextLowercased)
+        }
+    }
+}
+
+struct PostListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostListView()
+    }
+}
